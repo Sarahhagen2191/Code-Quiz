fcc8f94d87cdfb73a7a1defe565283483cef9a7a
Sarah hagen <sarahhagen@sarahs-mbp.home>
2022-12-06 20:17:34 +0000

    update values for query selector

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,24 +1,30 @@
-//Value for Timer//
+var currentQuestionIndex = 0;
 
-var currentQuestionIndex = 0; 
-var timer = document.querySelector('#time');
-var timeLeft = 60;
-var timerWrapper = document.querySelector('.timer');
-var feedback = document.querySelector('#feedback');
+//Timer Values//
+var time = document.querySelector("#time");
+var timer = document.querySelector(".timer");
+var timeLeft= 60;
+
+var feedback = document.querySelector("#feedback");
+
+//Start page Values//
+var startButton = document.querySelector("#start");
+var startScreen = document.querySelector("#start-screen");
+
+//Quiz page Values//
+var questionWrap = document.querySelector("#questions");
+var questionTitle = document.querySelector("#question-title");
+var choicesQutput = document.querySelector("#choices");
+
+//End of Quiz page Values//
+var endScreen = document.querySelector("#end-screen");
+var finalScore = document.querySelector("#final-score");
+
+//Sumbit result Values//
+var initialsInput = document.querySelector("#initials");
+var submitButton = document.querySelector("#submit");
+var form = document.querySelector("form");
 
 
-
-//Value for start pages// 
-var startButton = document.querySelector('#start');
-
-var startScreen = document.querySelector('#start-screen');
-
-//Value for question page// 
-var questionWrapper = document.querySelector('#questions');
-var questionTitle = document.querySelector('#question-title');
-var choicesAnswers = document.querySelector('#choices');
-
-//Value for end quiz/finalscore // 
-var endScreen = document.querySelector('#end-screen');
-var finalScore = document.querySelector('#final-score');
-
+  
+  
